9e02bfa6ae2eaa320751db68c663928e87071018
goto0214 <yuusuke0214goto1997@outlook.jp>
2021-12-04 16:18:07 +0000

    [u@pdate]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,20 +7,27 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id)
+    flash[:notice] = "Book was successfully updated."
+    else
+      render :edit
+    end
   end
   def destroy
   book = Book.find(params[:id])
   book.destroy
   redirect_to books_path
+  flash[:notice] = "Book was successfully destroyed."
   end
   def create
-    @book = Book.new(params[:id])
+    @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully created."
     else
+      @books = Book.all
       render :index
     end
   end
